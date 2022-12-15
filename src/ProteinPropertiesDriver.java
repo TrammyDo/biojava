2dacff46a9da6929c534108c65f7335c2c04d6a8
Tram Do <do0588@pacificu.edu>
2022-12-14 19:31:01 -0800

    Added a scroll pane to the gui to set size of window

--- src/ProteinPropertiesDriver.java
+++ src/ProteinPropertiesDriver.java
@@ -31,24 +31,27 @@ public class ProteinPropertiesDriver {
     ProteinSequence proteinSequence;
     ImageIcon icon = new ImageIcon("C:\\Users\\John\\IdeaProjects\\biojavaTram\\img\\png-clipart-protein-structure-amino-acid-biology-proteins-s-text-structure-thumbnail.png");
 
-    uniProtId = (String) JOptionPane.showInputDialog(null,"Enter a UniProt ID",
+    uniProtId = (String) JOptionPane.showInputDialog(null, "Enter a UniProt ID",
             "Protein Theoretical Properties", JOptionPane.INFORMATION_MESSAGE, icon, null, "");
     JButton button = new JButton("Calculate!");
     button.addActionListener(new ButtonPress());
-
     uniProtProtein = new UniProtProtein(uniProtId);
     proteinSequence = uniProtProtein.getProtein();
     protein = uniProtProtein.getProtein().toString();
     getProperties = new ProteinTheoProps(protein);
 
-    UIManager.put("OptionPane.minimumSize",new Dimension(100,100));
-    JOptionPane.showMessageDialog(null, "Sequence: " + proteinSequence + "\nLength: " +
-                                  protein.length() + "\nMolecular Weight: " + getProperties.getMolecularWeight() +
-                                  " Da" + "\nIsolectric Point: " + df.format(getProperties.getIsoelectricPoint()) +
-                                  " pH" + "\nHydrophobicity: " + df.format(getProperties.getHydrophobicity()) +
-                                  "\nExtinction coefficient 1: " + df.format(getProperties.getExtinctionCoefficient1()) +
-                                  "\nExtinction coefficient 2: " + df.format(getProperties.getExtinctionCoefficient2()),
-                              "Protein Theoretical Properties", JOptionPane.PLAIN_MESSAGE);
+    String output[] = {"Sequence: " + proteinSequence, "\nLength: " + protein.length(),
+                       "\nMolecular Weight: " + getProperties.getMolecularWeight() + " Da",
+                       "\nIsolectric Point: " + df.format(getProperties.getIsoelectricPoint()) + " pH",
+                       "\nHydrophobicity: " + df.format(getProperties.getHydrophobicity()),
+                       "\nExtinction coefficient 1: " + df.format(getProperties.getExtinctionCoefficient1()),
+                       "\nExtinction coefficient 2: " + df.format(getProperties.getExtinctionCoefficient2())};
+
+    JList list = new JList(output);
+    JScrollPane scrollPane = new JScrollPane(list);
+    scrollPane.setPreferredSize(new Dimension(500, 500));
+    JOptionPane.showMessageDialog(null, scrollPane, "Protein Theoretical Properties",
+                                  JOptionPane.PLAIN_MESSAGE);
   }
 }
 
